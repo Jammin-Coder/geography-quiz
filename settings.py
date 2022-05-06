7184038b82cdb2a1c2fdad712b7dea4f6ecce17f
jammincoder <jammincoderguy96@gmail.com>
2022-05-06 14:59:35 -0400

    Modified settings

---
+++ settings.py
@@ -0,0 +1,18 @@
+MAPS_DIR = 'static/maps'
+JSON_DIR = 'static/json'
+CONTINENTS = ['north america', 'south america', 'africa', 'europe', 'asia', 'oceania']
+
+GITHUB_USERNAME = 'Jammin-Coder'
+GITHUB_REPO = 'geography-quiz'
+
+
+# Database
+DB_HOST = 'localhost'
+DB_USERNAME = 'USERNAME'
+DB_PASSWORD = 'PASSWORD'
+DB_NAME = 'DATABASE_NAME'
+
+CONTINENTS_TABLE_NAME = 'continents'
+CONTINENT_NAME_COLUMN = 'continent_name'
+JSON_COUNTRIES_COLUMN = 'json_countries'
+
