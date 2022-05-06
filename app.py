3f4a5287b37630c8e31d02da0a7789aeef6e3376
jammincoder <jammincoderguy96@gmail.com>
2022-05-05 23:51:20 -0400

    App is back to stable state

--- app.py
+++ app.py
@@ -1,27 +1,27 @@
 from flask import Flask, render_template
-from utils import get_iso_name, get_random_territory_from_region
-from utils import get_continents
+from settings import CONTINENTS
+from utils import get_iso_from_country, get_random_country_from_continent
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    continents = get_continents()
     images = dict()
-    for continent in continents:
+    for continent in CONTINENTS:
         images[continent] = f'/static/maps/{continent}/{continent}.png'
 
     return render_template('index.html', images=images)
 
 @app.route('/<continent>/quiz')
 def quiz(continent):
-    territory_name = get_random_territory_from_region(continent)
-    territory_iso = get_iso_name(territory_name)
+    country = get_random_country_from_continent(continent)
+    country_name = country['country']
+    country_iso = country['iso']
     territory_image_file = '256.png'
-    territory_image_url = f'/static/maps/{continent}/{territory_iso}/{territory_image_file}'.replace(' ', '%20')
+    territory_image_url = f'/static/maps/{continent}/{country_iso}/{territory_image_file}'.replace(' ', '%20')
 
     territory = {
-        "name": territory_name,
+        "name": country_name,
         "image_url": territory_image_url
     }
     
